5078df5908db650c06ffb7e4a85878812c85f7c9
sairam-1023 <sairamamaraneni22@gmail.com>
2026-05-05 18:23:39 +0530

    my change

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,7 @@ export default function Home() {
               style={{ color: '#111827' }}
               className="flex-1 border border-gray-300 rounded-lg px-3 py-2 text-sm
                          text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400
-                         placeholder:text-gray-900"
+                         placeholder:text-gray-400"
             />
             <button
               onClick={handleAsk}
